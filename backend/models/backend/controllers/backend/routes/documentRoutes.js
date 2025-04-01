78875d19d402d15e9248ee752b1c0b3925655685
dkmapks <dkmap039@gmail.com>
2025-04-01 16:38:37 +0200

    Create documentRoutes.js

---
+++ backend/models/backend/controllers/backend/routes/documentRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { createDocument, getDocuments } = require('../controllers/documentController');
+
+const router = express.Router();
+
+router.post('/', createDocument);
+router.get('/', getDocuments);
+
+module.exports = router;
